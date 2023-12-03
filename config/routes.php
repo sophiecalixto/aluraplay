2790837acd831a17e3f2ef73bcce2359c996f208
Sophie Calixto <sophiecalixto2004@gmail.com>
2023-12-03 13:06:16 -0300

    feat: routes

---
+++ config/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'GET|/' => "/../view/home.php",
+    'GET|/novo-video' =>  "/../view/form.php",
+    'POST|/novo-video' => "/../controller/AddVideoController.php",
+    'GET|/editar-video' => "/../view/form.php",
+    'POST|/editar-video' => "/../controller/UpdateVideoController.php",
+    'GET|/remover-video' =>  "/../controller/DeleteVideoController.php"
+];
--- public/index.php
+++ public/index.php
@@ -2,20 +2,22 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-if($_SERVER['PATH_INFO'] == '/' || !array_key_exists('PATH_INFO', $_SERVER)) {
-    require_once __DIR__ . '/../view/home.php';
-} elseif ($_SERVER['PATH_INFO'] == '/novo-video') {
-    if($_SERVER['REQUEST_METHOD'] == 'GET') {
-        require_once __DIR__ . '/../view/form.php';
-    } elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        require_once __DIR__ . '/../controller/AddVideoController.php';
+$routes = require_once(__DIR__ . '/../config/routes.php');
+
+$method = $_SERVER['REQUEST_METHOD'];
+$pathInfo = $_SERVER['PATH_INFO'] ?? '/';
+
+foreach ($routes as $route => $handler) {
+    list($routeMethod, $routePath) = explode('|', $route);
+
+    if ($method === $routeMethod && $pathInfo === $routePath) {
+        if (file_exists($handler)) {
+            require_once(__DIR__ . $handler);
+        } else {
+            require_once(__DIR__ . '/../view/home.php');
+        }
+        exit();
     }
-} elseif ($_SERVER['PATH_INFO'] == '/editar-video') {
-    if($_SERVER['REQUEST_METHOD'] == 'GET') {
-        require_once __DIR__ . '/../view/form.php';
-    } elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        require_once __DIR__ . '/../controller/UpdateVideoController.php';
-    }
-} elseif ($_SERVER['PATH_INFO'] == "/remover-video") {
-    require_once __DIR__ . '/../controller/DeleteVideoController.php';
 }
+
+echo "Rota não encontrada";
