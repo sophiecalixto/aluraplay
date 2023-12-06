c6e857ffbdea167bb04ca35447c9501fc23ea04b
Sophie Calixto <sophiecalixto2004@gmail.com>
2023-12-05 21:24:31 -0300

    feat: image mime type verif

--- model/Image.php
+++ model/Image.php
@@ -7,10 +7,17 @@ class Image
     public static function saveImage(array $formUpload): ?string
     {
         if ($formUpload['error'] == UPLOAD_ERR_OK) {
+            $filePath = $formUpload['tmp_name'];
             $image =  uniqid() . $formUpload['name'];
+            $fileInfo = new \finfo(FILEINFO_MIME_TYPE);
+            $mimeType = $fileInfo->file($filePath);
+
+            if(!str_starts_with($mimeType, 'image/')) {
+                return null;
+            }
             if (
                 move_uploaded_file(
-                    $formUpload['tmp_name'],
+                    $filePath,
                     __DIR__ . '/../public/img/uploads/' . $image
                 )
             ) {
